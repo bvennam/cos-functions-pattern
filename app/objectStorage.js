11da1045d2cb39ae519564cdc49c026dc7a967f5
Belinda Vennam <beemarie88@gmail.com>
2019-06-09 11:25:49 -0400

    object storage creds cleanup

--- app/objectStorage.js
+++ app/objectStorage.js
@@ -1,24 +1,18 @@
 const COS_SDK = require('ibm-cos-sdk');
 const config = require('./config.js');
+const creds = require('./credentials.json');
 
-let cosCredentials; 
+let cosCredentials;
 
 if (process.env.OBJECTSTORAGE_CREDENTIALS) {
-  console.log('Found Object Storage credentials in OBJECTSTORAGE_CREDENTIALS env var')
+  console.log('Found Object Storage credentials in OBJECTSTORAGE_CREDENTIALS env var');
   cosCredentials = JSON.parse(process.env.OBJECTSTORAGE_CREDENTIALS);
 } else {
   console.log('Missing env var OBJECTSTORAGE_CREDENTIALS, using credentials.json');
-  cosCredentials = require('./credentials.json').OBJECTSTORAGE_CREDENTIALS
+  cosCredentials = creds.OBJECTSTORAGE_CREDENTIALS;
 }
 
-//  var cos_config =   {
-//   'endpoint': config.EndPointURL,
-//   'apiKeyId': cosCredentials.apikey,
-//   'ibmAuthEndpoint': 'https://iam.ng.bluemix.net/oidc/token',
-//   'serviceInstanceId': cosCredentials.resource_instance_id,
-// };
-
-const cos_config = {
+const cosConfig = {
   endpoint: config.EndPointURL,
   apiKeyId: cosCredentials.apikey,
   ibmAuthEndpoint: 'https://iam.ng.bluemix.net/oidc/token',
@@ -27,7 +21,7 @@ const cos_config = {
   credentials: new COS_SDK.Credentials(cosCredentials.cos_hmac_keys.access_key_id, cosCredentials.cos_hmac_keys.secret_access_key, sessionToken = null),
   signatureVersion: 'v4',
 };
-const cos = new COS_SDK.S3(cos_config);
+const cos = new COS_SDK.S3(cosConfig);
 
 
 module.exports = cos;
